9fcbc3b1978b118a4a4273f0c431ef47cd28a108
opportify-fp <192269140+opportify-fp@users.noreply.github.com>
2025-02-01 11:22:02 -0800

    feat: update email api

--- lib/v1/models/AnalyzeEmail200Response.ts
+++ lib/v1/models/AnalyzeEmail200Response.ts
@@ -2,7 +2,7 @@
 /* eslint-disable */
 /**
  * Opportify Insights API
- * ## Overview  The **Opportify Insights API** provides access to a powerful and up-to-date platform. With advanced data warehousing and AI-driven capabilities, this API is designed to empower your business to make informed, data-driven decisions and effectively assess potential risks.  ### Base URL Use the following base URL for all API requests:  ```plaintext https://api.opportify.ai/insights/v1/<service>/<endpoint> ```  ### Features - [**Email Insights:**](/docs/api/api-reference/email-insights)   - Validate email syntax.   - Identify email types (free, disposable, corporate or unknown).   - Real time verifications:     - Reachable: Confirms if the email domain has valid MX DNS records using DNS lookup.     - Deliverable: Simulates an SMTP handshake to check if the email address exists and is deliverable.     - Catch-All: Detects if the domain accepts all emails (catch-all configuration).   - Intelligent Error Correction: Automatically corrects well-known misspelled email addresses.   - Risk Report: Provides an AI-driven normalized score (200-1000) to evaluate email risk, using predefined thresholds.      [Access Documentation >>](/docs/api/api-reference/email-insights)  - [**IP Insights:**](/docs/api/api-reference/ip-insights)   - Connection types: Detects connection types such as `wired`, `mobile`, `enterprise`, `satellite`, `VPN`, `cloud-provider`, `open-proxy`, or `Tor`.   - Geo location: Delivers detailed insights such as country, city, timezone, language preferences, and additional location-based information to enhance regional understanding.   - WHOIS: Provides main details including RIR, ASN, organization, and abuse/admin/technical contacts.   - Trusted Provider Recognition: Identifies if the IP is part of a known trusted provider (e.g., ZTNA - Zero Trust Network Access).   - Blocklist Reports: Retrieves up-to-date blocklist statuses, active reports, and the latest detections.   - Risk Report: Delivers an AI-driven normalized score (200-1000) to evaluate IP risk, supported by predefined thresholds.    [Access Documentation >>](/docs/api/api-reference/ip-insights)  ### Authentication & Security - **API Key:** Access to the API requires an API key, which must be included in the request headers. Businesses can generate unlimited API keys directly from their account, offering flexibility and ease of use.  - **ACL Rules:** Enhance security with Access Control Lists (ACL), allowing you to restrict API access from specific IP addresses or ranges. This feature provides an additional layer of protection by ensuring only authorized IPs can interact with the API. - **No Query Parameters:** As a precautionary measure, our API avoids the use of query parameters for all operations, including authentication and handling Personally Identifiable Information (PII). This approach minimizes security risks by preventing sensitive data from being exposed in access logs, browser history, cached URLs, debugging tools, or inadvertently shared URLs. All sensitive information is securely transmitted through headers or the request body. 
+ * ## Overview  The **Opportify Insights API** provides access to a powerful and up-to-date platform. With advanced data warehousing and AI-driven capabilities, this API is designed to empower your business to make informed, data-driven decisions and effectively assess potential risks.  ### Base URL Use the following base URL for all API requests:  ```plaintext https://api.opportify.ai/insights/v1/<service>/<endpoint> ```  ### Features - [**Email Insights:**](/docs/api/api-reference/email-insights)   - Validate email syntax.   - Identify email types (free, disposable, private or unknown).   - Real time verifications:     - Reachable: Confirms if the email domain has valid MX DNS records using DNS lookup.     - Deliverable: Simulates an SMTP handshake to check if the email address exists and is deliverable.     - Catch-All: Detects if the domain accepts all emails (catch-all configuration).   - Intelligent Error Correction: Automatically corrects well-known misspelled email addresses.   - Risk Report: Provides an AI-driven normalized score (200-1000) to evaluate email risk, using predefined thresholds.      [Access Documentation >>](/docs/api/api-reference/email-insights)  - [**IP Insights:**](/docs/api/api-reference/ip-insights)   - Connection types: Detects connection types such as `wired`, `mobile`, `enterprise`, `satellite`, `VPN`, `cloud-provider`, `open-proxy`, or `Tor`.   - Geo location: Delivers detailed insights such as country, city, timezone, language preferences, and additional location-based information to enhance regional understanding.   - WHOIS: Provides main details including RIR, ASN, organization, and abuse/admin/technical contacts.   - Trusted Provider Recognition: Identifies if the IP is part of a known trusted provider (e.g., ZTNA - Zero Trust Network Access).   - Blocklist Reports: Retrieves up-to-date blocklist statuses, active reports, and the latest detections.   - Risk Report: Delivers an AI-driven normalized score (200-1000) to evaluate IP risk, supported by predefined thresholds.    [Access Documentation >>](/docs/api/api-reference/ip-insights)  ### Authentication & Security - **API Key:** Access to the API requires an API key, which must be included in the request headers. Businesses can generate unlimited API keys directly from their account, offering flexibility and ease of use.  - **ACL Rules:** Enhance security with Access Control Lists (ACL), allowing you to restrict API access from specific IP addresses or ranges. This feature provides an additional layer of protection by ensuring only authorized IPs can interact with the API. - **No Query Parameters:** As a precautionary measure, our API avoids the use of query parameters for all operations, including authentication and handling Personally Identifiable Information (PII). This approach minimizes security risks by preventing sensitive data from being exposed in access logs, browser history, cached URLs, debugging tools, or inadvertently shared URLs. All sensitive information is securely transmitted through headers or the request body. 
  *
  * The version of the OpenAPI document: 1.0.0
  * 
@@ -20,13 +20,13 @@ import {
     EmailDNSToJSON,
     EmailDNSToJSONTyped,
 } from './EmailDNS';
-import type { RiskReport } from './RiskReport';
+import type { RiskReportEmail } from './RiskReportEmail';
 import {
-    RiskReportFromJSON,
-    RiskReportFromJSONTyped,
-    RiskReportToJSON,
-    RiskReportToJSONTyped,
-} from './RiskReport';
+    RiskReportEmailFromJSON,
+    RiskReportEmailFromJSONTyped,
+    RiskReportEmailToJSON,
+    RiskReportEmailToJSONTyped,
+} from './RiskReportEmail';
 
 /**
  * 
@@ -47,7 +47,7 @@ export interface AnalyzeEmail200Response {
      */
     emailProvider: string;
     /**
-     * Type of email address (e.g., free, disposable, corporate, unknown).
+     * Type of email address (e.g., free, disposable, private, unknown).
      * @type {string}
      * @memberof AnalyzeEmail200Response
      */
@@ -65,12 +65,12 @@ export interface AnalyzeEmail200Response {
      */
     emailCorrection: string;
     /**
-     * Checks if the email address exists and is deliverable using SMTP handshake simulation. This involves connecting to the mail server and issuing commands to verify deliverability.
+     * Checks if the email address exists and is deliverable using SMTP handshake simulation. This involves connecting to the mail server and issuing commands to verify deliverability. The possible answers are `yes`, `no`, or `unknown`. We guarantee a high confidence level on this parameter since this is a real time verification.
      * 
-     * @type {boolean}
+     * @type {string}
      * @memberof AnalyzeEmail200Response
      */
-    isDeliverable: boolean;
+    isDeliverable: string;
     /**
      * Determines if the email domain is configured as a catch-all, which accepts emails for all addresses within the domain. This is verified through multiple email tests.
      * 
@@ -78,6 +78,13 @@ export interface AnalyzeEmail200Response {
      * @memberof AnalyzeEmail200Response
      */
     isCatchAll: boolean;
+    /**
+     * Determines if the mailbox associated with the email is full, in association with isDeliverable field, it can give a reason why the email is not deliverable.
+     * 
+     * @type {boolean}
+     * @memberof AnalyzeEmail200Response
+     */
+    isMailboxFull: boolean;
     /**
      * Confirms if the email domain has valid MX DNS records using DNS lookup.
      * @type {boolean}
@@ -92,10 +99,10 @@ export interface AnalyzeEmail200Response {
     emailDNS: EmailDNS;
     /**
      * 
-     * @type {RiskReport}
+     * @type {RiskReportEmail}
      * @memberof AnalyzeEmail200Response
      */
-    riskReport: RiskReport;
+    riskReport: RiskReportEmail;
 }
 
 /**
@@ -109,6 +116,7 @@ export function instanceOfAnalyzeEmail200Response(value: object): value is Analy
     if (!('emailCorrection' in value) || value['emailCorrection'] === undefined) return false;
     if (!('isDeliverable' in value) || value['isDeliverable'] === undefined) return false;
     if (!('isCatchAll' in value) || value['isCatchAll'] === undefined) return false;
+    if (!('isMailboxFull' in value) || value['isMailboxFull'] === undefined) return false;
     if (!('isReachable' in value) || value['isReachable'] === undefined) return false;
     if (!('emailDNS' in value) || value['emailDNS'] === undefined) return false;
     if (!('riskReport' in value) || value['riskReport'] === undefined) return false;
@@ -132,9 +140,10 @@ export function AnalyzeEmail200ResponseFromJSONTyped(json: any, ignoreDiscrimina
         'emailCorrection': json['emailCorrection'],
         'isDeliverable': json['isDeliverable'],
         'isCatchAll': json['isCatchAll'],
+        'isMailboxFull': json['isMailboxFull'],
         'isReachable': json['isReachable'],
         'emailDNS': EmailDNSFromJSON(json['emailDNS']),
-        'riskReport': RiskReportFromJSON(json['riskReport']),
+        'riskReport': RiskReportEmailFromJSON(json['riskReport']),
     };
 }
 
@@ -156,9 +165,10 @@ export function AnalyzeEmail200ResponseToJSONTyped(value?: AnalyzeEmail200Respon
         'emailCorrection': value['emailCorrection'],
         'isDeliverable': value['isDeliverable'],
         'isCatchAll': value['isCatchAll'],
+        'isMailboxFull': value['isMailboxFull'],
         'isReachable': value['isReachable'],
         'emailDNS': EmailDNSToJSON(value['emailDNS']),
-        'riskReport': RiskReportToJSON(value['riskReport']),
+        'riskReport': RiskReportEmailToJSON(value['riskReport']),
     };
 }
 
