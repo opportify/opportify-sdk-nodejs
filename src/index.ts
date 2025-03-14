e99b16c8b67b7d5f19768cc41393c5468a148ba2
opportify-fp <192269140+opportify-fp@users.noreply.github.com>
2025-03-14 03:31:37 -0700

    feat: expose response types (#10)

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export { IPInsights } from './ipInsights';
 export { type AnalyzeIpRequest } from '../lib/v1/models/AnalyzeIpRequest';
 export { type Configuration } from '../lib/v1/runtime';
 export { type ErrorResponse } from './types';
+export type { AnalyzeEmail200Response } from '../lib/v1/models/AnalyzeEmail200Response';
+export type { AnalyzeIp200Response } from '../lib/v1/models/AnalyzeIp200Response';
